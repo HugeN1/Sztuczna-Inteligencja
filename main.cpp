0b478027b36091e1ff29af1146c0db647117066d
Przemysław Chmara <pcprzemek99@gmail.com>
2021-10-06 14:57:51 +0200

    testv3

--- main.cpp
+++ main.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
-float obliczenia(f1, f2, n){
-    max_x = f2(0);
-    max_y = f1(max_x);
-    for(i=1; i<n; i++)
+auto obliczenia = [](auto f1,auto f2,int n){
+   int max_x = f2(0);
+   int max_y = f1(max_x);
+    for(int i=1; i<n; i++)
 {
-        x = f2(i);
-        y = f1(x);
+        int x = f2(i);
+       int y = f1(x);
         if(y > max_y){
             max_x = x;
             max_y = y;
         }
 }
-    return max_x, max_y;
+    return (max_x, max_y);
 }
 
-float f2(i){
-        (2*(i%2)-1)*i/2
+auto f2 = [](int i){
+int a = (2*(i%2)-1)*i/2;
+        return a;
 };
 
-float f1(x){
+auto f1 = [](int x){
     20-abs(x);
+    return x;
 };
 
 int main() {
-      cout<< max_x;
-    cout<< max_y;
+    auto [a,b] = obliczenia(f2, 10);
+      cout<< a;
+    cout<< b;
     return 0;
 }
