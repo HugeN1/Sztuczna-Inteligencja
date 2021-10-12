bf33cf80d5c076c2ed40472a2c26b4a2239dbfa0
Przemysław Chmara <pcprzemek99@gmail.com>
2021-10-12 14:08:33 +0200

    zad1 nai

--- app1.1.cpp
+++ app1.1.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
-int main (){
-    cout<<"qwerty"
-    return 0;
+auto obliczenia = [](auto f1,auto f2,int n){
+    int max_x = f2(0);
+    int max_y = f1(max_y);
+    for(int i = 1; i < n; i++)
+    {
+        int x = f2(i);
+        int y = f1(x);
+        if(y > max_y){
+            max_x = x;
+            max_y = y;
+        }
+    }
+    return make_pair(max_x, max_y);
 };
+
+auto f2 = [](int i){
+    int a = (2*(i%2)-1)*i/2;
+    return a;
+};
+
+auto f1 = [](int x){
+    20-abs(x);
+    return x;
+};
+
+int main(int argc, char **argv) {
+    auto [a,b] = obliczenia(f1, f2, 10);
+    cout<< a << "|" << b <<endl;
+}
 
